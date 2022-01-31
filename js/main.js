14a307247b91acfb831807bae01e13e577d8b896
ibrohim777775 <ibrohim777775@gmail.com>
2022-01-31 15:27:21 +0500

    added media.css and mobile menu

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const burgerBtn = document.querySelector(".header__mob-btn");
+const mobileMenu = document.querySelector("#mob__menu");
+const closeBtn = document.querySelector(".header__mob-close-btn");
+
+burgerBtn.addEventListener("click", () => {
+  console.log("bosildi");
+  mobileMenu.classList.toggle("active");
+  burgerBtn.classList.toggle("falsy");
+  closeBtn.classList.toggle("show__close-btn");
+});
